1a694f97f2b8c0af1bd70b9d9ba35f844ea5c1d6
friedaTV <38824640+friedatv@users.noreply.github.com>
2018-04-29 05:41:43 +0200

    StartUp

---
+++ GG.py
@@ -0,0 +1,13 @@
+# Impor - Modul: pygame
+import pygame
+
+# Import - Modul: Spielclient
+
+# PyGame festlegen
+pygame.init()
+
+# Client festlegen
+Client = None
+
+# Programmschleife festlegen
+Client.programmschleife()
---
+++ Konfiguration/Variablen.py
@@ -0,0 +1,5 @@
+# Import - Modul: os
+import os
+
+# Pfad - Grundverzeichnis festlegen
+Pfad_Grundverzeichnis = os.path.realpath('')
